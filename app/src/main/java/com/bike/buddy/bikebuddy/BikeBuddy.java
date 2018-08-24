daaaef39a8865ed3384b10bdd0f660bed250541a
bijesh shrestha <bijesh.shrestha.90@gmail.com>
2018-08-24 13:40:48 -0600

    saving data to singleton

--- app/src/main/java/com/bike/buddy/bikebuddy/BikeBuddy.java
+++ app/src/main/java/com/bike/buddy/bikebuddy/BikeBuddy.java
@@ -2,9 +2,17 @@ package com.bike.buddy.bikebuddy;
 
 import android.app.Application;
 
+import com.bike.buddy.bikebuddy.retrofit.model.Network;
+import com.bike.buddy.bikebuddy.retrofit.model.NetworksResponse;
+
+import java.util.List;
+
 public class BikeBuddy extends Application{
 
     private static BikeBuddy mInstance;
+
+    public NetworksResponse networks;
+    public List<Network> filteredNetworks;
 
     @Override
     public void onCreate() {
