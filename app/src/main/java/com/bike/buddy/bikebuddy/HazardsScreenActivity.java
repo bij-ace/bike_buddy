22d360cd27a49b9104699919e852884483fa2d70
preeti1989 <preeti.1989@outlook.com>
2018-08-24 11:59:03 -0400

    Created HazardsScreenActivity and ListResultActivity

---
+++ app/src/main/java/com/bike/buddy/bikebuddy/HazardsScreenActivity.java
@@ -0,0 +1,13 @@
+package com.bike.buddy.bikebuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HazardsScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hazards_screen);
+    }
+}
